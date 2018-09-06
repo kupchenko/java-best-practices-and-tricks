346c6d98ae3cf53865f066c06cf76efb1e3621da
Dmitrii Kupchenko <kupchenko.dmitrii@gmail.com>
2018-09-06 12:35:10 +0300

    Tricks: Thread - synchronization with implicit notify() invocation

---
+++ src/main/java/me/kupchenko/thread/SynchronizationOnThread.java
@@ -0,0 +1,14 @@
+package me.kupchenko.thread;
+
+public class SynchronizationOnThread {
+    private final static Thread thread = new Thread(()-> System.out.println("Hello there!!!"));
+
+    public static void main(String[] args) throws InterruptedException {
+        synchronized (thread) {
+            thread.start();
+            thread.wait();
+            // Java invokes notifyAll() automatically???!!!
+        }
+        System.out.println("Hello after???!!!");
+    }
+}
